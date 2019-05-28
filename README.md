5a9506ce7899abc1d309501de4884b1e9d1fa88a
jef king <jef@microsoft.com>
2019-05-28 09:34:23 -0700

    fixing up functions

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# fn-call-webhook
+# Azure Functions
+Diving deep into a hack-a-thon, with Functions.
+fakeload (test) -> API -> Worker -> fakehook (test)
 
 ## Deployment
 az group deployment create --name deploying --resource-group oss --template-file azure-deploy.json --parameters azure-deploy.params.json
--- fakeload/index.js
+++ fakeload/index.js
@@ -1,10 +1,18 @@
 module.exports = function (context, req) {
     context.log('generate data');
 
+    let num = Math.floor(Math.random() * 10);
     let m = {
-        id: 5,
-        name: 'happy'
+        id: num,
+        unique: uuidv4()
     };
     
     context.done(null, m);
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
