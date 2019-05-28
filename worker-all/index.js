b7520439446e07c05f459db32129501a2f212c5f
jef king <jef@microsoft.com>
2019-05-28 12:10:26 -0700

    log enqueued time

--- worker-all/index.js
+++ worker-all/index.js
@@ -1,5 +1,6 @@
 module.exports = function (context, message) {
     context.log('logging called');
+    context.log('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc);
     context.log(message);
 
     context.done();
