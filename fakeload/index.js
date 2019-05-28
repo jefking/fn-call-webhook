d5d987c62e6ce32760262d057f941edcfb2812c1
jef king <jef@microsoft.com>
2019-05-28 10:44:43 -0700

    cron post re-write

--- fakeload/index.js
+++ fakeload/index.js
@@ -1,53 +1,25 @@
-var querystring = require('querystring');
-var http = require('http');
+var request = require('request');
 
 //THIS IS JUST FOR TESTING
 module.exports = function (context, req) {
     context.log('generating mock data');
 
     let num = Math.floor(Math.random() * 10);
-    let m = {
-        id: num,
-        unique: uuidv4(),
-        code: "Kbx5r5MAbW3ZiWvvaPCLCceNXbjXHq56CgUnqze3tD/XEP2rN3F50g=="//HACKY WORK AROUND
-    };
 
-    context.log(m);
-    context.log(querystring.stringify(m));
-    
-    PostCode(m);
-
-    context.done();
-}
-
-function PostCode(model) {
-    // Build the post string from an object
-    var post_data = querystring.stringify(model);
-  
-    // An object of options to indicate where to post to
-    var post_options = {
-        host: 'deepthunker.azurewebsites.net',
-        port: '443',
-        path: '/api/api',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(post_data)
+    request.post(
+        'deepthunker.azurewebsites.net/api/api?code=Kbx5r5MAbW3ZiWvvaPCLCceNXbjXHq56CgUnqze3tD/XEP2rN3F50g==',
+        {
+            json: {
+                id: num,
+                unique: uuidv4()
+            }
+        },
+        function (error, response, body) {
+            if (error) context.done(error);
+            else context.done(null, body);
         }
-    };
-  
-    // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
-    });
-  
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
-  }
+    );
+}
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
