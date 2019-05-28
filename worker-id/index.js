59c57a5894638c01f92108f64d9c487f4f6c36ef
jef king <jef@microsoft.com>
2019-05-28 11:06:31 -0700

    send to fake hook

--- worker-id/index.js
+++ worker-id/index.js
@@ -2,5 +2,15 @@ module.exports = function (context, message) {
     context.log('webhook caller called');
     context.log(message);
 
+    request.post('https://deepthunker.azurewebsites.net/api/fakehook?code=n8ac5oliqJyqreC7wOpbvRE71WpKAY8nfLRC4z9diNVHRrVyXsagLg==',
+        {
+            json: true,
+            body: message
+        },
+        function (error, response, body) {
+            if (error) context.done(error);
+        }
+    );
+
     context.done();
 }
