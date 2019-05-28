ec8a56d321f06004074ce78a795f1b726793e319
jef king <jef@microsoft.com>
2019-05-28 12:13:05 -0700

    remove extraa logging

--- worker-all/index.js
+++ worker-all/index.js
@@ -1,6 +1,4 @@
 module.exports = function (context, message) {
-    context.log('logging called');
-    context.log('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc);
     context.log(message);
 
     context.done();
