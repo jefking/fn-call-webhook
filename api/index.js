236105aa1c3e87ee41654f20327af4168be1028e
jef king <jef@microsoft.com>
2019-05-27 22:05:58 -0700

    empty api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
+const azure = require('azure-sb');
+
 module.exports = function (context, req) {
     context.log('api called');
+
+    let model = {};
+    var brokeredMessage = {
+        body: JSON.stringify(model),
+        customProperties: {
+            //Add custom properties to filter on with Subscriptions
+            'id':'2'
+        }
+    }
+
+    let serviceBusService = azure.createServiceBusService(process.env.ServiceBus);
+    serviceBusService.sendTopicMessage(process.env.TopicName, brokeredMessage, function (error) {
+    });
+
+    context.res = {
+        status: 200
+    };
+
+    context.done();
 }
