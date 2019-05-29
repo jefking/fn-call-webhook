6d9d95653b06de2747df6703668d0c65efe89178
jef king <jef@microsoft.com>
2019-05-29 13:56:30 -0700

    trace request

--- worker-all/index.js
+++ worker-all/index.js
@@ -2,15 +2,29 @@ const request = require('request');
 const appInsights = require("applicationinsights");
 
 module.exports = function (context, msg) {
-    context.log(msg);
+    appInsights.setup().start(); // assuming ikey is in env var
+    let aiClient = appInsights.defaultClient;
 
-    request.post('https://deepthunker.azurewebsites.net/api/fake-hook?code=6XErItfUWT/nTVB8ksEa2JLxCOkZDQ1hbbatczBWpRYtCThZL63GNA==',
+    let url = 'https://deepthunker.azurewebsites.net/api/fake-hook?code=6XErItfUWT/nTVB8ksEa2JLxCOkZDQ1hbbatczBWpRYtCThZL63GNA==';
+    request.post(url,
         {
             json: true,
             body: msg
         },
         function (error, response, body) {
-            if (error) context.done(error);
+            if (error)
+            {
+                aiClient.trackException({exception: error});
+                context.done(error);
+            }
+            else
+            {
+                context.log(response);
+
+                context.log(body);
+
+                client.trackRequest({name:"GET /customers", url:url, duration:309, resultCode:200, success:true});
+            }
         }
     );
 
