a762ec2e3589ac4b40f27a5a623083f69a39f45f
jef king <jef@microsoft.com>
2019-05-29 12:36:31 -0700

    remove silly things.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,6 @@
-// const { ServiceBusClient } = require("@azure/service-bus");
-
-// const sbClient = ServiceBusClient.createFromConnectionString(process.env.ServiceBus); 
-// const topicClient = sbClient.createTopicClient(process.env.TopicName);
-// const sender = topicClient.createSender();
-
 module.exports = async function (context, req) {
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
     let err = !model ? "no data; or invalid payload in body" : null;
-
-    context.log(model);
 
     if (!err) {
         const timeNowUtc = new Date(Date.now());
