21b6a0994e685578b3778225029cc5cb34d06d64
jef king <jef@microsoft.com>
2019-05-29 14:56:00 -0700

    post dude

--- worker-all/index.js
+++ worker-all/index.js
@@ -5,33 +5,28 @@ module.exports = function (context, msg, registration) {
     appInsights.setup().start();
     let aiClient = appInsights.defaultClient;
 
-    context.log({reg: registration});
-    // let config = JSON.parse(registration);
-
-    // context.log({config: config});
-    let url = registration.url;//'https://deepthunker.azurewebsites.net/api/fake-hook?code=6XErItfUWT/nTVB8ksEa2JLxCOkZDQ1hbbatczBWpRYtCThZL63GNA==';
+    let url = registration.url;
     
-    context.log({url: url});
-    // request.post(url,
-    //     {
-    //         json: true,
-    //         body: msg
-    //     },
-    //     function (error, response, body) {
-    //         if (error)
-    //         {
-    //             context.done(error);
-    //         }
-    //         else
-    //         {
-    //             context.log({rep: response});
+    request.post(url,
+        {
+            json: true,
+            body: msg
+        },
+        function (error, response, body) {
+            if (error)
+            {
+                context.done(error);
+            }
+            else
+            {
+                context.log({rep: response});
 
-    //             context.log({body: body});
+                context.log({body: body});
 
-    //             client.trackRequest({name:"POST", url:url, duration:309, resultCode:200, success:true});
-    //         }
-    //     }
-    // );
+                client.trackRequest({name:"POST", url:url, duration:309, resultCode:200, success:true});
+            }
+        }
+    );
 
     context.done();
 }
