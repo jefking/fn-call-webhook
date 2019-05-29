8fb431f69cde6c23a51d373977e8a6cdcf89aaee
jef king <jef@microsoft.com>
2019-05-29 10:47:23 -0700

    test

--- api/index.js
+++ api/index.js
@@ -27,7 +27,9 @@ module.exports = async function (context, req) {
 
         // context.log(msg);
 
-        await sender.scheduleMessages(scheduledEnqueueTimeUtc, {body: "wtf"});
+        //await sender.scheduleMessages(scheduledEnqueueTimeUtc, {body: "wtf"});
+
+        await sender.send({body: "wtf"});
         //context.bindings.send = msg;
     }
     
