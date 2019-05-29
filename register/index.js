3d412e7f09c3113e3b23a150ab606fa8c84e934e
jef king <jef@microsoft.com>
2019-05-29 16:28:58 -0700

    fixing code

--- register/index.js
+++ register/index.js
@@ -4,6 +4,7 @@ module.exports = async function (context, req) {
     let err = !model.url ? "no url" : null;
     let err = !model.action ? "no action" : null;
     let err = !model.resource ? "no resource" : null;
+    let err = !model.tenantId ? "no tenand id" : null;
 
     context.res = {
         status: err ? 500 : 200,
