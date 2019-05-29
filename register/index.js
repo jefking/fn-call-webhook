94be8851df21af4b206e6e1d121161f455263c34
jef king <jef@microsoft.com>
2019-05-29 16:34:51 -0700

    errors

--- register/index.js
+++ register/index.js
@@ -1,17 +1,18 @@
 module.exports = async function (context, req) {
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
-    let err = !model ? "no data; or invalid payload in body" : null;
-    let err = !model.url ? "no url" : null;
-    let err = !model.action ? "no action" : null;
-    let err = !model.resource ? "no resource" : null;
-    let err = !model.tenantId ? "no tenand id" : null;
+    let err = ''
+    err += !model ? "no data; or invalid payload in body" : null;
+    err += !model.url ? "no url" : null;
+    err += !model.action ? "no action" : null;
+    err += !model.resource ? "no resource" : null;
+    err += !model.tenantId ? "no tenand id" : null;
 
     context.res = {
-        status: err ? 500 : 200,
+        status: err != '' ? 500 : 200,
         body: err
     };
 
     context.bindings.json = model;
     
-    context.done(err);
+    context.done(err != '' ? err : null);
 };
