7856aa2bb4feca17fcab35b7d11288cc861c5a67
jef king <jef@microsoft.com>
2019-05-29 14:40:58 -0700

    comment out action

--- worker-all/index.js
+++ worker-all/index.js
@@ -8,26 +8,26 @@ module.exports = function (context, msg, registration) {
     context.log({reg: registration});
 
     let url = registration.url;//'https://deepthunker.azurewebsites.net/api/fake-hook?code=6XErItfUWT/nTVB8ksEa2JLxCOkZDQ1hbbatczBWpRYtCThZL63GNA==';
-    request.post(url,
-        {
-            json: true,
-            body: msg
-        },
-        function (error, response, body) {
-            if (error)
-            {
-                context.done(error);
-            }
-            else
-            {
-                context.log({rep: response});
+    // request.post(url,
+    //     {
+    //         json: true,
+    //         body: msg
+    //     },
+    //     function (error, response, body) {
+    //         if (error)
+    //         {
+    //             context.done(error);
+    //         }
+    //         else
+    //         {
+    //             context.log({rep: response});
 
-                context.log({body: body});
+    //             context.log({body: body});
 
-                client.trackRequest({name:"POST", url:url, duration:309, resultCode:200, success:true});
-            }
-        }
-    );
+    //             client.trackRequest({name:"POST", url:url, duration:309, resultCode:200, success:true});
+    //         }
+    //     }
+    // );
 
     context.done();
 }
