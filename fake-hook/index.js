0551f6c5210d07fba7879334b05e595185f9bbc9
Jef King <jef@spud.ca>
2019-11-24 15:18:29 -0800

    store successes

--- fake-hook/index.js
+++ fake-hook/index.js
@@ -5,6 +5,7 @@ module.exports = function (context, req) {
     appInsights.setup().start();
     
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
+    context.save = model;
     context.log(model);
     
     context.res = {
