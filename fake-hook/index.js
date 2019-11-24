58f799846b5d9347fb76b104db98003029330339
Jef King <jef@spud.ca>
2019-11-24 15:40:10 -0800

    logging everything

--- fake-hook/index.js
+++ fake-hook/index.js
@@ -4,10 +4,13 @@ const appInsights = require("applicationinsights");
 module.exports = function (context, req) {
     appInsights.setup().start();
     
-    context.save = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
-    
+    let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
+    context.save = model;
+    context.log(model);
+
     context.res = {
-        status: 200
+        status: 200,
+        body: model
     };
 
     context.done();
