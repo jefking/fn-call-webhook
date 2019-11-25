0d4d5cbdb10596cd8e8acebcd2e97e4cc798872e
Jef King <jef@spud.ca>
2019-11-24 17:13:13 -0800

    binding should work

--- fake-hook/index.js
+++ fake-hook/index.js
@@ -6,8 +6,6 @@ module.exports = function (context, req) {
     
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
 
-    context.bindings.json = model.body;
-    context.log(context.bindings.json.id);
     context.res = {
         status: 200
     };
