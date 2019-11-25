b9ecf83961b7b66e71671c51ecba0ac2771ad693
Jef King <jef@spud.ca>
2019-11-24 16:38:01 -0800

    log id

--- fake-hook/index.js
+++ fake-hook/index.js
@@ -6,12 +6,12 @@ module.exports = function (context, req) {
     
     let model = (typeof req.body != 'undefined' && typeof req.body == 'object') ? req.body : null;
 
-    context.log(model.body.id);
-    context.bindings.json = model.body;
-    context.bindings.json.id = model.body.id;
+    // context.log(model.body.id);
+    // context.bindings.json = model.body;
+    // context.bindings.json.id = model.body.id;
     context.res = {
         status: 200
     };
 
-    context.done();
+    context.done(model.body);
 }
